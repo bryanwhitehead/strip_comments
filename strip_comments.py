b03c94c8634fd8a82d9938ef2b962a8b1fdafe3b
Bryan Whitehead <78087276+bryanwhitehead@users.noreply.github.com>
2021-03-30 11:49:28 +0200

    added spaces

--- strip_comments.py
+++ strip_comments.py
@@ -4,10 +4,9 @@
 def solution(string,markers):
     #print(string) this 
     output = ""
-
     i = 0
     length = len(string)                                      # string length 
-    while (i < length):                                      # process entire string                       
+    while i < length:                                      # process entire string                       
         if string[i] == ' ' and string[i+1] == '\n':
             output += '\n'
             i += 1
